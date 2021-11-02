f8d6f77b82f7060d2b648cb2821bb8431167ce56
Housam-j <housam-j@hotmail.com>
2021-11-02 19:12:57 +0000

    done

--- typewriter.js
+++ typewriter.js
@@ -1,8 +1,7 @@
 const sentence = "hello there from lighthouse labs \n";
-let delay = 1000;
+let delay = 100;
 for (const char of sentence) {
-    setTimeout(() => {
-      process.stdout.write(char);
-    // print the char here
-  }, delay +=50) // <= 1s delay to make it noticeable. Can dial it down later.
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay += 50);
 }
